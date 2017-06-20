dc8bc0912ff1bc64a28f2908481191bff201d569
Guillaume Rose <guillaume.rose@docker.com>
2017-06-20 12:51:50 +0200

    Add wait-for-it
    Signed-off-by: Guillaume Rose <guillaume.rose@docker.com>

--- 17.06-rc/dind/dockerd-entrypoint.sh
+++ 17.06-rc/dind/dockerd-entrypoint.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 set -e
 
-# no arguments passed
-# or first arg is `-f` or `--some-option`
-if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
-	# add our default arguments
-	set -- dockerd \
+dockerd \
 		--host=unix:///var/run/docker.sock \
 		--host=tcp://0.0.0.0:2375 \
-		--storage-driver=vfs \
-		"$@"
-fi
+		--insecure-registry 10.0.0.0/8 \
+		--storage-driver=vfs &
 
-if [ "$1" = 'dockerd' ]; then
-	# if we're running Docker, let's pipe through dind
-	# (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
-	set -- sh "$(which dind)" "$@"
-fi
-
-exec "$@"
+wait-for-it.sh 0.0.0.0:2375
+$@
